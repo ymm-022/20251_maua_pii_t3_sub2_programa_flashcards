cd603b60d814b2d0f5b753f96c0951633f97af94
Julio Murata <muratajulio320@gmail.com>
2025-05-29 08:50:29 -0300

    update

---
+++ src/main/java/br/imt/pimaua/persistencia/ConnectionFactory.java
@@ -0,0 +1,33 @@
+
+package br.imt.pimaua.persistencia;
+
+import java.sql.Connection ;
+import java.sql.DriverManager ;
+
+public class ConnectionFactory {
+    private String host = "mysql-gyozadb-pii-20251-maua.i.aivencloud.com" ;
+    private String port = "24545" ;
+    private String db = "20251_MAUA_PII_FLASHCARDS" ;
+    private String user = "avnadmin" ;
+    private String password = "AVNS_SkxHbc0w6rZJyVV_5XY" ;
+    
+    public Connection obterConexao() throws Exception{
+        var s = String.format(
+        "jdbc:mysql://%s:%s/%s",
+        host, port, db 
+        ) ;
+        Connection c = DriverManager.getConnection(s, user, password) ;
+        return c ;
+    }
+    
+    public static void main(String [] args) throws Exception{
+        var fabricaDeConexoes = new ConnectionFactory() ;
+        Connection conexao = fabricaDeConexoes.obterConexao() ;
+        if(conexao != null){
+            System.out.println("Conectou!") ;
+        }
+        else{
+            System.out.println("Não conectou...") ;
+        }
+    }
+}
