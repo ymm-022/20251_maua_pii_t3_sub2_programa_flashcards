42131a320981e4627e4fc4ede15da0456da42d62
Gustavo Moncayo Correa <gugamonca@gmail.com>
2025-06-10 18:40:08 -0300

    adiciona idusuario

--- src/main/java/br/imt/pimaua/Usuario.java
+++ src/main/java/br/imt/pimaua/Usuario.java
@@ -1,14 +1,20 @@
 package br.imt.pimaua;
 
 public class Usuario {
+    private int idusuario;
     private String nome;
     private String email;
     private int senha;
 
-    public Usuario( String nome,String email, int senha) {
+    public Usuario( int idusuario, String nome, String email, int senha) {
+        this.idusuario = idusuario;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
+    }
+    
+    public int getIdusuario() {
+        return idusuario;
     }
 
     public String getNome() {
